dd5ba3bb8cecbbde8074330b902cb56ecfb2d6bf
Tommy Leung <me@supertommy.com>
2018-07-06 16:39:16 -0600

    remove unused Start method

--- Assets/Scripts/GamePauseButton.cs
+++ Assets/Scripts/GamePauseButton.cs
@@ -5,11 +5,6 @@ public class GamePauseButton : MonoBehaviour
 	[SerializeField]
 	private GameObject pauseCanvas;
 
-	private void Start() 
-	{
-		
-	}
-
 	public void HandlePause()
 	{
 		Instantiate(pauseCanvas);
